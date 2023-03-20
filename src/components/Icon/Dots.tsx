6d284dca79bd260883e5362b30a1c1340e720c63
Amy Haywood Dutton <me@amyhaywood.com>
2023-03-19 23:32:23 -0500

    ✨ Finished the Icon component

---
+++ src/components/Icon/Dots.tsx
@@ -0,0 +1,17 @@
+interface DotsProps {
+  height?: number;
+  width?: number;
+}
+
+const Dots = ({ height = 24, width = 24 }: DotsProps) => {
+  return (
+    <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <title>Dots</title>
+      <circle cx="4.5" cy="12" r="2.25" />
+      <circle cx="12" cy="12" r="2.25" />
+      <circle cx="19.5" cy="12" r="2.25" />
+    </svg>
+  )
+}
+
+export {Dots}
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,22 +1,45 @@
+import { Check } from "./Check";
+import { Close } from "./Close";
+import { Delete } from "./Delete";
+import { Discord } from "./Discord";
+import { Dots } from "./Dots";
+import { Edit } from "./Edit";
+import { Email } from "./Email";
+import { GitHub } from "./GitHub";
+import { LinkedIn } from "./Linkedin";
+import { Twitter } from "./Twitter";
+import { Upload } from "./Upload";
+
 interface IconProps {
   height?: number;
   name: string;
   width?: number;
 }
 
-const Icon = ({ height = 32, name, width = 32 }: IconProps) => {
+const Icon = ({ height = 24, name, width = 24 }: IconProps) => {
   switch (name.toLowerCase()) {
     case "check":
+      return <Check height={height} width={width} />
     case "close":
-    case "discord":
-    case "dots":
-    case "edit":
-    case "email":
+      return <Close height={height} width={width} />
     case "delete":
+      return <Delete height={height} width={width} />
+    case "discord":
+      return <Discord height={height} width={width} />
+    case "dots":
+      return <Dots height={height} width={width} />
+    case "edit":
+      return <Edit height={height} width={width} />
+    case "email":
+      return <Email height={height} width={width} />
     case "github":
+      return <GitHub height={height} width={width} />
     case "linkedin":
+      return <LinkedIn height={height} width={width} />
     case "twitter":
+      return <Twitter height={height} width={width} />
     case "upload":
+      return <Upload height={height} width={width} />
     default:
       return <div />
   }
