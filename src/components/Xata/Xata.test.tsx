6d60413d09656151031797a8e00068b27e58ca01
Amy Haywood Dutton <me@amyhaywood.com>
2023-03-21 21:34:31 -0500

    ✅ Wrote tests for Xata component

---
+++ src/components/Xata/Xata.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import { Xata } from './Xata';
+
+describe("Xata", () => {
+  it('renders correctly', () => {
+    expect(() => {
+      render(<Xata />)
+    }).not.toThrow();
+  });
+
+  it('matches the snapshot', () => {
+    expect(render(<Xata />)).toMatchSnapshot();
+  })
+});
