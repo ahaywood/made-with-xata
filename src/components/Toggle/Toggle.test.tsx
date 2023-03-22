3d9d451f0b35debbfb365c1a7d67c13c7677fdf8
Amy Haywood Dutton <me@amyhaywood.com>
2023-03-21 21:13:13 -0500

    ✅ Wrote tests for toggle component

--- src/components/Toggle/Toggle.test.tsx
+++ src/components/Toggle/Toggle.test.tsx
@@ -8,8 +8,25 @@ describe("Toggle", () => {
     }).not.toThrow();
   });
 
-  it.skip('displays the label correctly', () => { })
-  it.skip('it defaults to off', () => { })
-  it.skip('it defaults to on', () => { })
-  it.skip('it toggles on and off', () => { })
+  it('displays the label correctly', () => {
+    render(<Toggle name="yolo" label="my label" />)
+    expect(screen.getByText('my label')).toBeInTheDocument();
+  })
+
+  it('it defaults to off', () => {
+    render(<Toggle name="yolo" />)
+    expect(screen.getByRole('checkbox')).not.toBeChecked();
+  })
+
+  it('it defaults to on', () => {
+    render(<Toggle name="yolo" defaultChecked={true} />)
+    expect(screen.getByRole('checkbox')).toBeChecked();
+  })
+
+  it('it toggles on and off', () => {
+    render(<Toggle name="yolo" label={'my label'} />)
+    expect(screen.getByRole('checkbox')).not.toBeChecked();
+    screen.getByLabelText('my label').click();
+    expect(screen.getByRole('checkbox')).toBeChecked();
+  })
 });
