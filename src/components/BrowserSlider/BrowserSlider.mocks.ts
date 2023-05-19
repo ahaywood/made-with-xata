d348a1f3a36ade1e256ff3988fca88fee7565ed9
Amy Haywood Dutton <me@amyhaywood.com>
2023-05-19 04:28:59 -0500

    ✨ Added a link to the Browser Slider

--- src/components/BrowserSlider/BrowserSlider.mocks.ts
+++ src/components/BrowserSlider/BrowserSlider.mocks.ts
@@ -1,36 +1,42 @@
 export const slides = [
   {
     alt: 'A cute kitten',
+    slug: 'cute-kitten',
     src: 'http://placekitten.com/800/800',
     width: 800,
     height: 533,
   },
   {
     alt: 'Another cute kitten',
+    slug: 'another-cute-kitten',
     src: 'http://placekitten.com/800/533',
     width: 800,
     height: 533,
   },
   {
     alt: 'The cutest kitten',
+    slug: 'the-cutest-kitten',
     src: 'http://placekitten.com/800/2080',
     width: 800,
     height: 533,
   },
   {
     alt: 'A cute kitten',
+    slug: 'cute-kitten-2',
     src: 'http://placekitten.com/800/800',
     width: 800,
     height: 533,
   },
   {
     alt: 'Another cute kitten',
+    slug: 'another-cute-kitten-2',
     src: 'http://placekitten.com/800/533',
     width: 800,
     height: 533,
   },
   {
     alt: 'The cutest kitten',
+    slug: 'the-cutest-kitten-2',
     src: 'http://placekitten.com/800/2080',
     width: 800,
     height: 533,
--- src/components/BrowserSlider/BrowserSlider.tsx
+++ src/components/BrowserSlider/BrowserSlider.tsx
@@ -1,14 +1,17 @@
+import Link from 'next/link';
 import { Browser, type BrowserProps } from '../Browser/Browser';
 
 interface BrowserSliderProps {
-  slides: BrowserProps[];
+  slides: (BrowserProps & { slug: string })[];
 }
 
 const BrowserSlider = ({ slides }: BrowserSliderProps) => (
   <div className="flex gap-x-[60px] snap-mandatory snap-x pb-5 overflow-x-scroll px-96">
-    {slides.map((slide: BrowserProps, index: number) => (
+    {slides.map((slide, index: number) => (
       <div className="snap-center" key={index}>
-        <Browser key={index} {...slide} />
+        <Link href={slide.slug}>
+          <Browser key={index} {...slide} />
+        </Link>
       </div>
     ))}
   </div>
